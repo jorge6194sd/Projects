a4b4e312c039bafbaefbae76f6c2e80d7094bb13
Jorge Vasquez <vasquezjorgito619@gmail.com>
2024-07-14 15:23:22 -0700

    added files

---
+++ VendingMachine/VendingMachine/Logic/ISodaMachineLogic.cs
@@ -0,0 +1,39 @@
+﻿using SodaMachineLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VendingMachine.Logic
+{
+    public interface ISodaMachineLogic
+    {
+        List<SodaModel> ListTypesOfSoda();
+
+        // Takes in an amount and returns the total deposited so far
+        decimal MoneyInserted(decimal monetaryAmount);
+
+        // Gets the overall price for sodas - there is no individual pricing
+        decimal GetSodaPrice();
+
+        // SodaModel (or null), List<Coin> Change, string ErrorMessage
+        (SodaModel soda, List<CoinModel> change, string errorMessage) RequestSoda(SodaModel soda);
+
+        void IssueFullRefund();
+
+        decimal GetMoneyInsertedTotal();
+
+        void AddToSodaInventory(List<SodaModel> sodas);
+
+        List<SodaModel> GetSodaInventory();
+
+        decimal EmptyMoneyFromMachine();
+
+        List<CoinModel> GetCoinInventory();
+
+        void AddToCoinInventory(List<CoinModel> coins);
+
+        decimal GetCurrentIncome();
+
+        decimal GetTotalIncome();
+    }
+}
