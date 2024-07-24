4f3b61d8d9cc23b58bc5f56a877f73700852b8bd
Jorge Vasquez <vasquezjorgito619@gmail.com>
2024-07-23 20:06:01 -0700

    added necessary testing nuget packages

--- VendingMachine/Testing/SodaLogicTests.cs
+++ VendingMachine/Testing/SodaLogicTests.cs
@@ -24,5 +24,93 @@ namespace Testing
             Assert.Throws<NotImplementedException>(() => testInterface.EmptyMoneyFromMachine());
         }
 
+        [Fact]
+        public void GetCoinInventory_ShouldThrowNotImplementedException()
+        {
+            //ARRANGE
+
+            //ACT
+
+            //ASSERT
+            Assert.Throws<NotImplementedException>(() => testInterface.GetCoinInventory());
+        }
+
+        [Fact]
+        public void GetCurrentIncome_ShouldThrowNotImplementedException()
+        {
+            //ARRANGE
+
+            //ACT
+
+            //ASSERT
+            Assert.Throws<NotImplementedException>(() => testInterface.GetCurrentIncome());
+        }
+
+        [Fact]
+        public void GetMoneyInsertedTotal_ShouldThrowNotImplementedException()
+        {
+            //ARRANGE
+
+            //ACT
+
+            //ASSERT
+            Assert.Throws<NotImplementedException>(() => testInterface.GetMoneyInsertedTotal());
+        }
+
+        [Fact]
+        public void GetSodaInventory_ShouldThrowNotImplementedException()
+        {
+            //ARRANGE
+
+            //ACT
+
+            //ASSERT
+            Assert.Throws<NotImplementedException>(() => testInterface.GetSodaInventory());
+        }
+
+        [Fact]
+        public void GetSodaPrice_ShouldThrowNotImplementedException()
+        {
+            //ARRANGE
+
+            //ACT
+
+            //ASSERT
+            Assert.Throws<NotImplementedException>(() => testInterface.GetSodaPrice());
+        }
+
+        [Fact]
+        public void GetTotalIncome_ShouldThrowNotImplementedException()
+        {
+            //ARRANGE
+
+            //ACT
+
+            //ASSERT
+            Assert.Throws<NotImplementedException>(() => testInterface.GetTotalIncome());
+        }
+
+        [Fact]
+        public void IssueFullRefund_ShouldThrowNotImplementedException()
+        {
+            //ARRANGE
+
+            //ACT
+
+            //ASSERT
+            Assert.Throws<NotImplementedException>(() => testInterface.IssueFullRefund());
+        }
+
+        [Fact]
+        public void ListTypesOfSoda_ShouldThrowNotImplementedException()
+        {
+            //ARRANGE
+
+            //ACT
+
+            //ASSERT
+            Assert.Throws<NotImplementedException>(() => testInterface.ListTypesOfSoda());
+        }
+
     }
 }
--- VendingMachine/Testing/obj/Debug/net8.0/Testing.AssemblyInfo.cs
+++ VendingMachine/Testing/obj/Debug/net8.0/Testing.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Testing")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca2e06497a8b51a2bf33d441092d2b208f800d2e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ec3bdf23e5ca8da545098d2ccbaa233fa304a1a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Testing")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Testing")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
